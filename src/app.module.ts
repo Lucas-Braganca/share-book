384f4918ea93b0009e53c70b13c3d80162d4d3ed
Lucas <lucas.braganca@clubpetro.com>
2021-12-04 17:00:49 -0300

    :sparkles: Creating books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { BooksModule } from './modules/books/books.module';
 import * as config from './ormconfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(config), UsersModule],
+  imports: [TypeOrmModule.forRoot(config), UsersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
