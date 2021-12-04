f099b9ceb7d7ddef80805514210604385e748c36
Lucas <lucas.braganca@clubpetro.com>
2021-12-03 21:58:10 -0300

    :sparkles: Swagger install

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const options = new DocumentBuilder()
+    .setTitle('Campaigns')
+    .setDescription('Campaigns')
+    .setVersion('1.0')
+    .addTag('Campaigns')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .addServer(`http://localhost:${PORT}`)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(PORT, HOST);
+
+  const logger = new Logger('Main');
+  logger.verbose(`REST application running on: ${await app.getUrl()}`);
 }
 bootstrap();
