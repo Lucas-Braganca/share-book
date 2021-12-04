705f8a3437f6e9559094d84cc2b2324edc93b231
Lucas <lucas.braganca@clubpetro.com>
2021-12-04 14:37:54 -0300

    :sparkles: Creating User module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
 import * as config from './ormconfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
