61912d491532c6d5d442846dd1a1a4df30be8745
Lucas <lucas.braganca@clubpetro.com>
2021-12-05 18:23:55 -0300

    :fire: Removing files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { BooksModule } from './modules/books/books.module';
 import { LoansModule } from './modules/loans/loans.module';
-import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 import * as config from './ormconfig';
 @Module({
-  imports: [TypeOrmModule.forRoot(config), UsersModule, BooksModule, LoansModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    UsersModule,
+    BooksModule,
+    LoansModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
